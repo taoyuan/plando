e26842dd8e3500266f5951f8247982441d981c99
taoyuan <torworx@gmail.com>
2015-07-14 20:06:43 +0800

    refactor

--- index.js
+++ index.js
@@ -4,26 +4,30 @@
 var assert = require('assert');
 
 function Plan(count, done) {
-  this.done = done;
-  this.count = count;
+    if (!(this instanceof Plan)) {
+        return new Plan(count, done);
+    }
+    this.done = done;
+    this.count = count;
 
-  if (0 === count) {
-    done();
-  }
+    if (0 === count) {
+        done();
+    }
 }
 
-Plan.prototype.ok = function(expression) {
-  assert(expression);
+Plan.prototype.ok = function (expression) {
+    assert(expression);
 
-  if (this.count === 0) {
-    assert(false, 'Too many assertions called');
-  } else {
-    this.count--;
-  }
+    if (this.count === 0) {
+        assert(false, 'Too many assertions called');
+    } else {
+        this.count--;
+    }
 
-  if (this.count === 0) {
-    this.done();
-  }
+    if (this.count === 0) {
+        this.done();
+    }
 };
 
-module.exports = Plan;
+exports.plan = Plan;
+
